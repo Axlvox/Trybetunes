9dd6b2f95a876cf8213c187cb925037d1a5de8ae
Axlvox <leandro.jlds@gmail.com>
2023-06-27 07:11:30 -0300

    Requisito 4

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Search from './pages/Search';
 import Album from './pages/Album';
+// import Header from './pages/Header';
+import Layout from './pages/Layout';
 import './index.css';
 // import Loading from './pages/Loading';
 // const navigate = useNavigate();
@@ -11,8 +13,10 @@ function App() {
     <div>
       <Routes>
         <Route path="/" element={ <Login /> } />
-        <Route path="/search" element={ <Search /> } />
-        <Route path="/album/:id" element={ <Album /> } />
+        <Route element={ <Layout /> }>
+          <Route path="/search" element={ <Search /> } />
+          <Route path="/album/:id" element={ <Album /> } />
+        </Route>
       </Routes>
       <p>Trybetunes</p>
     </div>
